892fa7799c9350f959a9e433ba560e5724bd94a0
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2026-05-05 01:31:31 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if (s1.length > s2.length) return false;
+        let hash1 = Array(26).fill(0)
+        let hash2 = Array(26).fill(0)
+
+        for (let i = 0; i < s1.length; i++){
+            ++hash1[s1.charCodeAt(i) - 97]
+            ++hash2[s2.charCodeAt(i) - 97]
+        }
+
+       
+
+        let i = 0
+        for (let j = s1.length - 1; j < s2.length; j++){
+            // console.log(s2[j])
+            if (this.isSubstring(hash1, hash2)){
+                return true
+            } else {
+                --hash2[s2.charCodeAt(i) - 97];
+                ++i
+                ++hash2[s2.charCodeAt(j + 1) - 97];
+            }
+
+        }
+
+        return false;
+
+    }
+
+    isSubstring(hash1, hash2){
+        for (let i = 0; i < hash1.length; i++){
+            if (hash1[i] !== hash2[i]){
+                return false
+            }
+        }
+
+        return true;
+    }
+}
