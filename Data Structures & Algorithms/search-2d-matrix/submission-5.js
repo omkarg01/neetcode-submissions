f63c743a4da29a192872bd54b71a94077f2776ee
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2026-05-06 01:54:25 +0530

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+
+        let rows = matrix.length; 
+        let cols = matrix[0].length;
+        let l = 0;
+        let r = rows * cols - 1
+
+        // console.log(r, c)
+
+        while (l <= r) {
+            let mid = Math.floor((r + l)/2)
+
+            // convert to 2D
+
+            let row = Math.floor(mid / cols)
+            let col = mid % cols
+            console.log(row, col)
+
+            if (matrix[row][col] === target){
+                return true
+            } else if (matrix[row][col] > target){
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+
+        }
+
+        return false
+
+
+    }
+}
